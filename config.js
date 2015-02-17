55be670806c3044b69155f3c6004e65c307d6cee
mc <mc@mcs-MacBook-Pro.local>
2015-02-16 21:16:40 -0800

    Adds changes to config.js

--- config.js
+++ config.js
@@ -11,7 +11,16 @@ config = {
     // Configure your URL and mail settings here
     production: {
         url: 'http://mattconrad.co/',
-        mail: {},
+        mail: {
+            transport: 'SMTP',
+            options: {
+                service: 'Gmail',
+                auth: {
+                    user: 'matthew.malloy.conrad@gmail.com',
+                    pass: 'lEdorlE86^^Go'
+                }
+            }
+        },
         database: {
             client: 'sqlite3',
             connection: {
